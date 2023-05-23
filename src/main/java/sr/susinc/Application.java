b0cd1088486da4dea4b1225fc7834717e4fa2ffd
dmartodikromo <dmartodikromo>
2023-05-23 01:34:15 +0000

    Initial commit

---
+++ src/main/java/sr/susinc/Application.java
@@ -0,0 +1,10 @@
+package sr.susinc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
